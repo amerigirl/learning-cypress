47aee94212965f4a7affeb335d1b3b30a9d7a028
Be-Asia McKerracher <amerigirl29@gmail.com>
2024-06-10 11:00:12 -0700

    checking connection

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,10 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig ({
   e2e: {
     baseUrl: 'http://localhost:4200',
-    
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+//sample check
